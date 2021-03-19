8615e3ad9dbe8fc49bea26243fae4994797caf0b
brs2287a <esteban.bravo111@gmail.com>
2021-03-19 16:58:25 +0100

    shadock collision intersect

--- app/src/main/java/com/m2dl/shadock/GameView.java
+++ app/src/main/java/com/m2dl/shadock/GameView.java
@@ -268,11 +268,11 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback, Sen
     }
 
     public void update() {
-        for (Ennemy e: ennemies) {
-            if ((e.getX() - 100 <= x && x <= e.getX() + 100) && (e.getY() - 100 <= y && y <= e.getY() + 100))
-                fin = true;
-        }
-        for(int i=0; i<bonuses.size();++i) {
+        //for (Ennemy e: ennemies) {
+        //  if ((e.getX() - 100 <= x && x <= e.getX() + 100) && (e.getY() - 100 <= y && y <= e.getY() + 100))
+        //    fin = true;
+        //}
+        for (int i = 0; i < bonuses.size(); ++i) {
             if ((bonuses.get(i).getX() - 100 <= x && x <= bonuses.get(i).getX() + 100) && (bonuses.get(i).getY() - 100 <= y && y <= bonuses.get(i).getY() + 100)) {
                 System.out.println(score);
                 score = score + 100;
@@ -310,7 +310,8 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback, Sen
         super.draw(canvas);
         if (canvas != null) {
             canvas.drawColor(Color.WHITE);
-            mCustomImage.setBounds(new Rect(x - 50, y - 75, x + 50, y + 75));
+            Rect shadok = new Rect(x - 50, y - 75, x + 50, y + 75);
+            mCustomImage.setBounds(shadok);
             mCustomImage.draw(canvas);
 
             if (rythm) {
@@ -329,16 +330,21 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback, Sen
                 shadokTired.setBounds(new Rect(150, 500, 650, 900));
                 shadokTired.draw(canvas);
             }
+
+            drawEnnemy(canvas, shadok);
+            drawBonus(canvas);
         }
-        drawEnnemy(canvas);
-        drawBonus(canvas);
     }
 
-    public void drawEnnemy(Canvas canvas) {
+    public void drawEnnemy(Canvas canvas, Rect shadok) {
         Paint paint = new Paint();
         paint.setColor(Color.rgb(0, 0, 255));
         for (Ennemy e : ennemies) {
-            fuseeGibi.setBounds(new Rect(e.getX(), e.getY(), e.getX() + 125, e.getY() + 200));
+            Rect giby = new Rect(e.getX(), e.getY(), e.getX() + 125, e.getY() + 200);
+            if (giby.intersect(shadok)) {
+                fin = true;
+            }
+            fuseeGibi.setBounds(giby);
             fuseeGibi.draw(canvas);
             //canvas.drawCircle(e.getX(), e.getY(), rayon, paint);
         }
@@ -382,21 +388,21 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback, Sen
     public void onSensorChanged(SensorEvent event) {
         float axisX = event.values[0];
         if (axisX > 7) {
-            acceleration = -6;
+            acceleration = -8;
         } else if (axisX > 5) {
-            acceleration = -4;
+            acceleration = -6;
         } else if (axisX > 3) {
-            acceleration = -2;
+            acceleration = -3;
         } else if (axisX > 1) {
-            acceleration = -1;
+            acceleration = -2;
         } else if (axisX < -7) {
-            acceleration = 6;
+            acceleration = 8;
         } else if (axisX < -5) {
-            acceleration = 4;
+            acceleration = 6;
         } else if (axisX < -3) {
-            acceleration = 2;
+            acceleration = 3;
         } else if (axisX < -1) {
-            acceleration = 1;
+            acceleration = 2;
         } else {
             acceleration = 0;
         }
